d0e4f9b2903c7bdb56cdec92a23ff90492b9d177
jinjing <995538274@qq.com>
2022-07-12 16:51:10 +0800

    2022/7/12

--- src/pages/Detail/ImageList/ImageList.vue
+++ src/pages/Detail/ImageList/ImageList.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="swiper-container">
+  <div class="swiper-container"ref="cur">
     <div class="swiper-wrapper">
-      <div class="swiper-slide">
-        <img src="../images/s1.png">
+      <div
+        class="swiper-slide" 
+        v-for="(silde, index) in skuImageList"
+        :key="silde.id"
+      >
+        <img :src="silde.imgUrl" :class="{active:currentIndex==index}"@click="changeIndex(index)"/>
       </div>
     </div>
     <div class="swiper-button-next"></div>
@@ -11,67 +15,102 @@
 </template>
 
 <script>
+import Swiper from "swiper";
+export default {
+  name: "ImageList",
+  data() {
+    return {
+      currentIndex:0
+    }
+  },
+  props: ["skuImageList"],
+  watch: {
+    // 数据监听，skuImageList是否发生变化，但是不能保证v-for遍历结构是否完整
+    skuImageList(newValue, oldValue) {
+      this.$nextTick(() => {
+         new Swiper(this.$refs.cur, {
+          // 如果需要前进后退按钮
+           navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
 
-  import Swiper from 'swiper'
-  export default {
-    name: "ImageList",
-  }
+          // 如果需要滚动条
+          scrollbar: ".swiper-scrollbar",
+          //显示几个图片设置
+           slidesPerView : 3,
+
+           slidesPerGroup : 1,
+        });
+      });
+    },
+  },
+  methods: {
+    changeIndex(index){
+      // 修改响应式数据
+      this.currentIndex = index;
+      // 通知兄弟组件：当前索引值为多少
+      this.$bus.$emit('getIndex',this.currentIndex)
+
+    }
+  },
+};
 </script>
 
 <style lang="less" scoped>
-  .swiper-container {
+.swiper-container {
+  height: 56px;
+  width: 412px;
+  box-sizing: border-box;
+  padding: 0 12px;
+
+  .swiper-slide {
+    width: 56px;
     height: 56px;
-    width: 412px;
-    box-sizing: border-box;
-    padding: 0 12px;
 
-    .swiper-slide {
-      width: 56px;
-      height: 56px;
+    img {
+      width: 100%;
+      height: 100%;
+      border: 1px solid #ccc;
+      padding: 2px;
+      width: 50px;
+      height: 50px;
+      display: block;
 
-      img {
-        width: 100%;
-        height: 100%;
-        border: 1px solid #ccc;
-        padding: 2px;
-        width: 50px;
-        height: 50px;
-        display: block;
-
-        &.active {
-          border: 2px solid #f60;
-          padding: 1px;
-        }
-
-        &:hover {
-          border: 2px solid #f60;
-          padding: 1px;
-        }
+      &.active {
+        border: 2px solid #f60;
+        padding: 1px;
       }
-    }
 
-    .swiper-button-next {
-      left: auto;
-      right: 0;
-    }
-
-    .swiper-button-prev {
-      left: 0;
-      right: auto;
-    }
-
-    .swiper-button-next,
-    .swiper-button-prev {
-      box-sizing: border-box;
-      width: 12px;
-      height: 56px;
-      background: rgb(235, 235, 235);
-      border: 1px solid rgb(204, 204, 204);
-      top: 0;
-      margin-top: 0;
-      &::after {
-        font-size: 12px;
-      }
+      // &:hover {
+      //   border: 2px solid #f60;
+      //   padding: 1px;
+      // }
     }
   }
+
+  .swiper-button-next {
+    left: auto;
+    right: 0;
+  }
+
+  .swiper-button-prev {
+    left: 0;
+    right: auto;
+  }
+
+  .swiper-button-next,
+  .swiper-button-prev {
+    box-sizing: border-box;
+    width: 12px;
+    height: 56px;
+    background: rgb(235, 235, 235);
+    border: 1px solid rgb(204, 204, 204);
+    top: 0;
+    margin-top: 0;
+    &::after {
+      font-size: 12px;
+    }
+  }
+}
 </style>
--- src/store/detail/index.js
+++ src/store/detail/index.js
@@ -16,11 +16,19 @@ const mutations = {
   }
 };
 const getters = {
+  // 路径导航简化的数据
   categoryView(state){
+    // 比如：state.goodInfo初始状态空对象，空对象的categroyView属性值undefined
+    // 当前计算出的categroyView属性值至少是一个空对象，警告不再有
     return state.goodInfo.categoryView ||{}
   },
+  // 简化产品信息的数据
   skuInfo(state){
     return state.goodInfo.skuInfo || {}
+  },
+  // 产品的售卖属性的简化
+  spuSaleAttrList(state){
+    return state.goodInfo.spuSaleAttrList ||[]
   }
 
 };
